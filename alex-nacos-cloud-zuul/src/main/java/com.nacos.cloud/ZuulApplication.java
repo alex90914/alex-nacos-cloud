cfefdf36f5cf87088bcfc86f10e5ed6a9fa9f317
wubo <wubo@artigent.cn>
2020-11-14 11:43:36 +0800

    alex-nacos-cloud init

---
+++ alex-nacos-cloud-zuul/src/main/java/com.nacos.cloud/ZuulApplication.java
@@ -0,0 +1,21 @@
+package org.aok.cloudname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author huangZhongYao
+ * @packageName org.aok.cloudname
+ * @date 2019/9/21
+ * @time 14:35
+ */
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
